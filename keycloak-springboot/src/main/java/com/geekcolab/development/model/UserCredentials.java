370d0fe395b00c59ca1aa67adbc5d3b1881196d1
Abdi T Bedaso <atbedaso@gmail.com>
2021-11-23 12:34:30 -0500

    imp crdential dto

---
+++ keycloak-springboot/src/main/java/com/geekcolab/development/model/UserCredentials.java
@@ -0,0 +1,18 @@
+package com.geekcolab.development.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserCredentials {
+
+	private String password;
+
+	private String username;
+
+}
